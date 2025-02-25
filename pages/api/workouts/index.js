39e95d87301ed5fa61a1d581269bbcd904b82142
3rlxnd <erlnd.gdt@gmail.com>
2025-02-25 10:41:43 +0100

    Changing 1

--- pages/api/workouts/index.js
+++ pages/api/workouts/index.js
@@ -3,8 +3,7 @@ import workouts from '../../../lib/resources/workouts.json'
 export default function handler(req, res) {
   if (req.method === 'GET') return res.status(200).json(workouts);
   if (req.method === 'POST') {
-    const { name, workouts } = req.body
-    console.log(req.body);
+    const { name, exercises } = req.body
     return res.status(200).json('Created Workout');
   }
   return res.status(405).json('Method not allowed')
