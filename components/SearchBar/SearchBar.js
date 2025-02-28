f880bdc5becaea4744bab26338d98921dc8f2ded
3rlxnd <erlnd.gdt@gmail.com>
2025-02-28 18:21:24 +0100

    FixedDezploment Issues

---
+++ components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function SearchBar() {
+  return (
+    <Container>
+        <Input type='text'>SearchBar</Input>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+`
+
+const Input = styled.input`
+`
