54b7f24d4bfa12ea85628e87ca3ad990b28ccc00
3rlxnd <erlnd.gdt@gmail.com>
2025-03-11 10:46:41 +0100

    Added Counter

--- components/DeleteModal/DeleteModal.js
+++ components/DeleteModal/DeleteModal.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default function DeleteModal({setDeleteVisible, handleDelete}) {
+export default function DeleteModal({ setDeleteVisible, handleDelete }) {
     // const [deleteVisible, setDeleteVisible] = useState(false);
 
     return (
